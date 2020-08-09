f4247f7276ae831a5065f72bd88c60861f6e8e4e
Wemerson Jesus <wejesuss@gmail.com>
2020-08-09 11:34:21 -0300

    Create routes navigation by tabs

---
+++ mobile/src/routes/StudyTabs.tsx
@@ -0,0 +1,81 @@
+import { Ionicons } from '@expo/vector-icons';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import React from 'react';
+import { Platform } from 'react-native';
+
+import Favorites from '../pages/Favorites';
+import TeacherList from '../pages/TeacherList';
+
+const { Navigator, Screen } = createBottomTabNavigator();
+
+const StudyTabs: React.FC = () => {
+  return (
+    <Navigator
+      tabBarOptions={{
+        style: {
+          elevation: 0,
+          shadowOpacity: 0,
+          height: Platform.OS === 'ios' ? 84 : 64,
+        },
+        tabStyle: {
+          flexDirection: 'row',
+          alignItems: 'center',
+          justifyContent: 'center',
+          paddingBottom: Platform.OS === 'ios' ? 20 : 0,
+        },
+        iconStyle: {
+          flex: 0,
+          width: 20,
+          height: Platform.OS === 'ios' ? 24 : 20,
+        },
+        labelStyle: {
+          fontFamily: 'Archivo_700Bold',
+          fontSize: 13,
+          marginLeft: 16,
+        },
+        inactiveBackgroundColor: '#fafafc',
+        activeBackgroundColor: '#ebebf5',
+        inactiveTintColor: '#c1bccc',
+        activeTintColor: '#32264d',
+        safeAreaInsets: {
+          bottom: 0,
+        },
+      }}
+    >
+      <Screen
+        name="TeacherList"
+        options={{
+          tabBarLabel: 'Proffys',
+          tabBarIcon: ({ color, size, focused }) => {
+            return (
+              <Ionicons
+                name="ios-easel"
+                color={focused ? '#8257e5' : color}
+                size={size}
+              />
+            );
+          },
+        }}
+        component={TeacherList}
+      />
+      <Screen
+        name="Favorites"
+        options={{
+          tabBarLabel: 'Favoritos',
+          tabBarIcon: ({ color, size, focused }) => {
+            return (
+              <Ionicons
+                name="ios-heart"
+                color={focused ? '#8257e5' : color}
+                size={size}
+              />
+            );
+          },
+        }}
+        component={Favorites}
+      />
+    </Navigator>
+  );
+};
+
+export default StudyTabs;
