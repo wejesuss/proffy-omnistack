ed61cdbc7df9c3894f66f170b3f714b36e9884ec
Wemerson Jesus <wejesuss@gmail.com>
2020-09-26 11:38:51 -0300

    refactor: add pragma to activate 'onDelete cascade'

--- server/src/database/connection.ts
+++ server/src/database/connection.ts
@@ -6,6 +6,14 @@ const db = knex({
     connection: {
         filename: resolve(__dirname, 'database.sqlite'),
     },
+    pool: {
+        afterCreate: (
+            conn: { run: (arg0: string, arg1: unknown) => void },
+            cb: unknown
+        ) => {
+            conn.run('PRAGMA foreign_keys = ON', cb);
+        },
+    },
     useNullAsDefault: true,
 });
 
