8a73b89c68d2fb782494cc969fd5d0da41d3df9a
Wemerson Jesus <wejesuss@gmail.com>
2020-09-26 11:41:59 -0300

    feat: add edit user route

--- server/src/routes/users.ts
+++ server/src/routes/users.ts
@@ -1,10 +1,13 @@
 import express from 'express';
 
+import UsersController from '../controllers/UsersController';
 import SessionController from '../controllers/SessionController';
 
 const routes = express.Router();
 
 routes.post('/create', SessionController.create);
+routes.put('/edit', UsersController.edit);
+
 routes.post('/login', SessionController.login);
 routes.post('/forgot-password', SessionController.forgot);
 routes.post('/reset-password', SessionController.reset);
