831cf9e801ffee41ec67244b1d7c04a5cfb85f58
Wemerson Jesus <wejesuss@gmail.com>
2020-10-01 10:34:04 -0300

    refactor: login page with hide/show password

--- web/src/pages/Login/index.tsx
+++ web/src/pages/Login/index.tsx
@@ -6,14 +6,18 @@ import { Link, useHistory } from 'react-router-dom';
 
 import Input from '../../components/Input';
 import LogoContainer from '../../components/LogoContainer';
-
-import './styles.css';
 import ControlContainer from '../../components/ControlContainer';
+
+import shownPassIcon from '../../assets/images/icons/show-password.svg';
+import hidedPassIcon from '../../assets/images/icons/hide-password.svg';
+import './styles.css';
 
 const Login: React.FC = () => {
   // const history = useHistory();
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [showPassword, setShowPassword] = useState(false);
+  const [remember, setRemember] = useState(false);
 
   function handleFormSubmit(e: FormEvent) {
     e.preventDefault();
@@ -30,6 +34,7 @@ const Login: React.FC = () => {
 
             <Input
               id="email"
+              leftBar
               name="email"
               label="E-mail"
               type="email"
@@ -40,19 +45,60 @@ const Login: React.FC = () => {
 
             <Input
               id="password"
+              leftBar
               name="password"
               label="Senha"
-              type="password"
+              type={showPassword ? 'text' : 'password'}
               upLabel={{ active: !!password }}
               value={password}
               onChange={(e) => setPassword(e.target.value)}
+              symbol={
+                showPassword ? (
+                  <button
+                    type="button"
+                    onClick={() => setShowPassword(!showPassword)}
+                  >
+                    <img src={shownPassIcon} alt="Ocultar senha" />
+                  </button>
+                ) : (
+                  <button
+                    type="button"
+                    onClick={() => setShowPassword(!showPassword)}
+                  >
+                    <img src={hidedPassIcon} alt="Mostrar senha" />
+                  </button>
+                )
+              }
             />
 
-            <Input type="checkbox" label="Lembrar-me" name="remember" />
+            <div className="control-box">
+              <button type="button" onClick={() => setRemember(!remember)}>
+                <div
+                  className={
+                    remember ? 'remember-checkbox active' : 'remember-checkbox'
+                  }
+                />
+              </button>
 
-            <Link to="/forgot-pswd">Esqueci minha senha</Link>
+              <Input
+                id="remember"
+                type="checkbox"
+                label="Lembrar-me"
+                name="remember"
+                checked={remember}
+                onChange={() => setRemember(!remember)}
+              />
 
-            <button type="submit">Entrar</button>
+              <Link to="/forgot-pswd">Esqueci minha senha</Link>
+            </div>
+
+            <button
+              type="submit"
+              disabled={!(email && password)}
+              className={email && password ? 'active' : ''}
+            >
+              Entrar
+            </button>
           </fieldset>
         </form>
       </ControlContainer>
